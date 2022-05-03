b12bc201a71e1ec3a85ba9191e1cc23ec9b2dcfa
Alexandru Preutesei <alexandru.preutesei@tajamar365.com>
2022-05-03 20:04:25 +0200

    Add project files.

---
+++ FileStorageAzure/Controllers/FileController.cs
@@ -0,0 +1,55 @@
+﻿using FileStorageAzure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileStorageAzure.Controllers
+{
+    public class FileController : Controller
+    {
+        private ServiceStorageFile service;
+
+        public FileController(ServiceStorageFile service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> IndexFiles()
+        {
+            List<string> files = await this.service.GetFilesAsync();
+            return View(files);
+        }
+        public async Task<IActionResult> ReadFile(string fileName)
+        {
+            string data = await this.service.ReadFileAsync(fileName);
+            ViewData["CONTENT"] = data;
+            return View();
+        }
+        public async Task<IActionResult> DeleteFile(string fileName)
+        {
+            await this.service.DeleteFileAsync(fileName);
+            return RedirectToAction("Index");
+        }
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            string fileName = file.FileName;
+            using (Stream stream = file.OpenReadStream())
+            {
+                await this.service.UploadFileAsync(fileName, stream);
+            }
+            ViewData["MENSAJE"] = "Fichero subido correctamente";
+            return View();
+        }
+
+    }
+}
---
+++ FileStorageAzure/Services/ServiceStorageFile.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Storage.Files.Shares;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileStorageAzure.Services
+{
+    public class ServiceStorageFile
+    {
+        private ShareDirectoryClient Root;
+
+        public ServiceStorageFile(string keys)
+        {
+            ShareClient client = new ShareClient(keys, "ficheros");
+            this.Root = client.GetRootDirectoryClient();
+        }
+
+        public async Task<List<string>> GetFilesAsync()
+        {
+            List<string> ficheros = new List<string>();
+            await foreach (var file in this.Root.GetFilesAndDirectoriesAsync())
+            {
+                ficheros.Add(file.Name);
+            }
+            return ficheros;
+        }
+
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            ShareFileClient file = this.Root.GetFileClient(fileName);
+            var data = await file.DownloadAsync();
+            Stream stream = data.Value.Content;
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string content = await reader.ReadToEndAsync();
+                return content;
+            }
+        }
+
+        public async Task UploadFileAsync(string fileName, Stream stream)
+        {
+            ShareFileClient file = this.Root.GetFileClient(fileName);
+            await file.CreateAsync(stream.Length);
+            await file.UploadAsync(stream);
+        }
+
+        public async Task DeleteFileAsync(string fileName)
+        {
+            ShareFileClient file = this.Root.GetFileClient(fileName);
+            await file.DeleteAsync();
+        }
+    }
+}
